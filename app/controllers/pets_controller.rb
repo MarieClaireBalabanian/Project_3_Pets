84d2151efb4ca639eeb8225539ee45e8fc8f1435
Marie-Claire Balabanian <marieclaire.balabanian@gmail.com>
2016-08-09 17:50:08 -0500

    pet profile view

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -10,44 +10,51 @@ class PetsController < ApplicationController
   #  POST - /pets/petprofile/ - Allows access to a single result of Ajax request
   #  -------------------------------------------------------------------------
   post '/petsave/?' do
-    @petid = params[:petdata]
+    @name        = params[:name]
+    @animal      = params[:animal]
+    @breed       = params[:breed]
+    @phone       = params[:phone]
+    @email       = params[:email]
+    @address     = params[:address]
+    @city        = params[:city]
+    @state       = params[:state]
+    @zip         = params[:zip]
+    @description = params[:description]
+    @picsmall    = params[:picsmall]
+    @petid       = params[:petid]
 
-    result  = HTTParty.get("http://api.petfinder.com/pet.get?key=61635e39395ce71e4d0eba82c79adb55&id=#{@petid}&format=json").parsed_response
-    pet = result["petfinder"]["pet"]
+    # result  = HTTParty.get("http://api.petfinder.com/pet.get?key=61635e39395ce71e4d0eba82c79adb55&id=#{@petid}&format=json").parsed_response
+    # pet = result["petfinder"]["pet"]
 
-    pet = {name:        pet["name"]["$t"],
-           type:        pet["animal"]["$t"],
-           description: pet["description"]["$t"],
-           phone:       pet["contact"]["phone"]["$t"],
-           email:       pet["contact"]["email"]["$t"],
-           address:     pet["contact"]["address1"]["$t"],
-           city:        pet["contact"]["city"]["$t"],
-           state:       pet["contact"]["state"]["$t"],
-           zip:         pet["contact"]["zip"]["$t"],
-           picsmall:    pet["media"]["photos"]["photo"][1]["$t"],
-           petid:       pet["id"]["$t"]
-          }
+    # pet = {name:        pet["name"]["$t"],
+    #        type:        pet["animal"]["$t"],
+    #        description: pet["description"]["$t"],
+    #        phone:       pet["contact"]["phone"]["$t"],
+    #        email:       pet["contact"]["email"]["$t"],
+    #        address:     pet["contact"]["address1"]["$t"],
+    #        city:        pet["contact"]["city"]["$t"],
+    #        state:       pet["contact"]["state"]["$t"],
+    #        zip:         pet["contact"]["zip"]["$t"],
+    #        picsmall:    pet["media"]["photos"]["photo"][1]["$t"],
+    #        petid:       pet["id"]["$t"]
+    #       }
 
-    pet = Pet.create name: pet[:name], address: pet[:address], phone: pet[:phone], website: pet[:email], userid: session[:user_id]
-
-    redirect '/profile/'
-  
+    # pet = Pet.create name: pet[:name], address: pet[:address], phone: pet[:phone], website: pet[:email], userid: session[:user_id]
+    puts @description
+    erb :pet
   end
+
+
 
   #  POST - /pets/ - Renders Ajax search results and map
   #                  sets params for us in Ajax request
   #  ---------------------------------------------------
   post '/?' do
-    puts session.id, '---session id'
-    puts session[:user_id], '---user id'
 
     @animal = params[:animal]
     @breed  = params[:breed]
     @zip    = params[:zip]
     
-    #    
-    # Calculate latitude and longitude for zip code entered by user
-    #-------------------------------------
     @zipCoord = Geocoder.coordinates(@zip)
     @zipLat = @zipCoord[0]
     @zipLng = @zipCoord[1]
@@ -55,9 +62,14 @@ class PetsController < ApplicationController
   
     result  = HTTParty.get("http://api.petfinder.com/pet.find?key=61635e39395ce71e4d0eba82c79adb55&location=#{@zip}&animal=#{@animal}&breed=#{@breed}&count=9&format=json").parsed_response
     
-    
     @petArray = []
     for animal in result["petfinder"]["pets"]["pet"] do
+
+
+
+
+
+      
       pet = {"name"        =>animal["name"]["$t"],
              "animal"      =>animal["animal"]["$t"],
              "breed"       =>@breed,
@@ -73,6 +85,8 @@ class PetsController < ApplicationController
              # "piclarge"    =>animal["media"]["photos"]["photo"][3]["$t"]
             }
 
+
+
     # if animal["media"]["photos"]["photo"][1]["$t"]
     #   pet[:picsmall] = animal["media"]["photos"]["photo"][1]["$t"]
     # else
@@ -84,15 +98,13 @@ class PetsController < ApplicationController
     #   pet[:piclarge] = animal["media"]["photos"]["photo"][2]["$t"]
     # else
     #   pet[:piclarge] = "No Photo"
-    # end
+    # end 
       @petArray.push(pet)
-    
+      puts pet["description"]
     end
-  
 
-    #
-    # Create arrays of location data from petfinder response to export to maps api
-    # ----------------------------------------------------------------------------
+
+
     @addressArray = []
     @nameArray = []
     @metaArray = [@nameArray, @addressArray]
